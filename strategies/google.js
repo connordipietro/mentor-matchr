2c7b222c17abac9500ebc906edd68faa802d84b6
Connor DiPietro <connor.dipietro@gmail.com>
2021-06-01 20:17:04 -0400

    adds: passport google strat import

---
+++ strategies/google.js
@@ -0,0 +1,11 @@
+const passport = require('passport');
+const { Strategy } = require('passport-google-oauth20');
+
+passport.use(new Strategy({
+  clientID: '',
+  clientSecret: '',
+  callbackURL: '',
+  scope: [] 
+}, (accessToken, refreshToken, profile, done) => {
+  
+}))
