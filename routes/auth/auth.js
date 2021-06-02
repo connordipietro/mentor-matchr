c140d71ad0e0fed9bd6726431992bf39994531f1
Connor DiPietro <connor.dipietro@gmail.com>
2021-06-01 20:01:47 -0400

    adds: auth routes for status, google login, and google redirect

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,10 +1,28 @@
 const router = require("express").Router();
 
-// /api/auth/
-router.get("/", (req, res) => {
+// -- /api/auth/
+
+// check login status
+router.get("/status", (req, res) => {
   res.send({ 
     status: 200, 
-    message: "test"})
+    message: "Status auth route"})
+});
+
+// auth with google
+router.get("/google", (req, res) => {
+  res.send({
+    status: 200,
+    message: "Google auth route"
+  })
+});
+
+// auth with google redirect
+router.get("/google/redirect", (req, res) => {
+  res.send({
+    status: 200,
+    message: "Google auth route"
+  })
 });
 
 module.exports = router;
