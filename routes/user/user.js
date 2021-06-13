16995e906058a4f86a173c0d632db51f03af5b40
connordipietro <74513418+connordipietro@users.noreply.github.com>
2021-06-13 19:59:11 -0400

    start match route

--- routes/user/user.js
+++ routes/user/user.js
@@ -1,6 +1,41 @@
 const router = require('express').Router();
 const user = require('../../database/models/user');
 
+// /api/user/matches
+/* router.get('/matches', async (req, res) => {
+  if (req.user) {
+    const { id } = req.user;
+      console.log(req.user); 
+    if (!id) return res.sendStatus(400);
+    const { mentorMentee, days, time, userInterests } = req.user.settings;
+
+     console.log(mentorMentee, days, time, userInterests);
+    console.log(mentorMentee.mentorMentee); 
+
+       const matches = await user
+      .find({ settings: { mentorMentee: !mentorMentee } })
+      .exec(); 
+
+    user.find({}, function (err, docs) {
+       console.log(docs);
+      const allUserSettings = docs.map((doc) => doc.settings);
+      console.log(allUserSettings); 
+      const filterByMentorMentee = allUserSettings.filter(
+        (doc) => doc.mentorMentee.mentorMentee !== mentorMentee.mentorMentee
+      );
+      // console.log(filterByMentorMentee);
+
+      const filterByTime = filterByMentorMentee.filter(
+        (doc) => doc.time.morning === days.time.morning
+      );
+      console.log(filterByTime);
+    });
+ 
+    return res.sendStatus(200);
+  }
+  return res.sendStatus(401);
+});
+ */
 // /api/user/account/create
 router.post('/account/create', async (req, res) => {
   if (req.user) {
