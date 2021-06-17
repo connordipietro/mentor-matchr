16814ea4c3f1e9d9968d2c8140a0bf80d2223b0e
connordipietro <74513418+connordipietro@users.noreply.github.com>
2021-06-17 15:17:33 -0400

    adds routing to pair programming page for non authed or set up users

--- client/src/pages/pair-program.js
+++ client/src/pages/pair-program.js
@@ -1,14 +1,16 @@
 import { PropTypes } from 'prop-types';
 import TextEditor from '../components/pair-programming/textEditor';
+import useIsAccountSetup from '../utilities/accountSetupHook';
+import useAuthCheck from '../utilities/authCheckHook';
 
 export const PairProgramming = (props) => {
   const { history } = props;
   const { state } = history.location;
+  const isAuth = useAuthCheck();
+  const isAccountSetup = useIsAccountSetup();
 
   return (
-    <>
-      <TextEditor userData={state} />
-    </>
+    <>{!isAuth || !isAccountSetup ? null : <TextEditor userData={state} />}</>
   );
 };
 
